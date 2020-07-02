643d7f2be439c323e28a1b659e19601421ec955a
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2020-07-02 17:23:01 -0300

    projeto 1, step 13

--- src/projeto1/Endereco.java
+++ src/projeto1/Endereco.java
@@ -7,4 +7,41 @@ public class Endereco {
 		private String estado;
 		private String cep;
 		private String complemento;
+		
+		public String getRua() {
+			return rua;
+		}
+		public void setRua(String rua) {
+			this.rua = rua;
+		}
+		public String getBairro() {
+			return bairro;
+		}
+		public void setBairro(String bairro) {
+			this.bairro = bairro;
+		}
+		public String getCidade() {
+			return cidade;
+		}
+		public void setCidade(String cidade) {
+			this.cidade = cidade;
+		}
+		public String getEstado() {
+			return estado;
+		}
+		public void setEstado(String estado) {
+			this.estado = estado;
+		}
+		public String getCep() {
+			return cep;
+		}
+		public void setCep(String cep) {
+			this.cep = cep;
+		}
+		public String getComplemento() {
+			return complemento;
+		}
+		public void setComplemento(String complemento) {
+			this.complemento = complemento;
+		}
 }
