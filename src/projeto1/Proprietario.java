0e77df29133d110a680974731b0ac6cff8e51d97
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2020-07-02 16:23:02 -0300

    projeto 1, step 2

--- src/projeto1/Proprietario.java
+++ src/projeto1/Proprietario.java
@@ -1,5 +1,14 @@
 package projeto1;
 
 public class Proprietario {
-
+	private String cpf;
+	private String nome;
+	private String rg;
+	private String dataDeNascimento;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+	private String complemento;
 }
