1ee020a45b7061375deab8a2c152e010f27320cf
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2020-07-02 16:38:21 -0300

    projeto 1, step 6

--- src/projeto1/Carro.java
+++ src/projeto1/Carro.java
@@ -1,5 +1,17 @@
 package projeto1;
 
 public class Carro {
-
+	private String modelo;
+	private String cor;
+	private String ano;
+	private String marca;
+	private String chassi;
+	private Proprietario proprietario;
+	private short velocidadeMaxima;
+	private short velocidadeAtual;
+	private byte numDePortas;
+	private boolean temTetoSolar;
+	private byte numDeMarchas;
+	private boolean temCambioAutomatico;
+	private short volumeDeCombustivel;
 }
